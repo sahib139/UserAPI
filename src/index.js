4957c77f8e3baf1481e35f163b68db9beeb8bd56
sahib singh <ss0990100@gmail.com>
2024-05-19 12:07:37 +0530

    upgrade http server to https server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const https = require("https");
+const fs = require("fs")
+const path = require("path")
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -9,8 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use("/api",APIroutes);
+app.use("/hey",(req,res)=>{res.status(200).json({
+    msg:"hey buddy",
+})})
 
-app.listen(PORT,async ()=>{    
+const sslServer = https.createServer({
+    key  : fs.readFileSync(path.join(__dirname,'..','cert','key.pem')),
+    cert : fs.readFileSync(path.join(__dirname,'..','cert','cert.pem')),
+},app);
+
+sslServer.listen(PORT,async ()=>{    
     console.log(`Server started at ${PORT}`);
     await dbConnect();
     console.log("database connected!");
